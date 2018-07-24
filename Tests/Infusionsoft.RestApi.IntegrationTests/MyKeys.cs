161201eb76bf12af9f45d3bb51767c13dffab1dc
Stefan <stefan.riedmann@ciclosoftware.com>
2018-07-24 08:53:56 -0300

    manage secrets

--- Tests/Infusionsoft.RestApi.IntegrationTests/MyKeys.cs
+++ Tests/Infusionsoft.RestApi.IntegrationTests/MyKeys.cs
@@ -2,7 +2,8 @@
 {
     public static class MyKeys
     {
-        
+        //after adding your keys, you should run this command to not commit your secrets
+        //git update-index --assume-unchanged Tests/Infusionsoft.RestApi.IntegrationTests/MyKeys.cs 
         public static string InfusionsoftClientId => "get it from developer.infusionsoft.com";
         public static string InfusionsoftClientSecret => "get it from developer.infusionsoft.com";
     }
